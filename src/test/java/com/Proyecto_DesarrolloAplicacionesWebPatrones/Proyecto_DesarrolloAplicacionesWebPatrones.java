c636fa8c89e82466265886b117db9fff71b6e307
jcsg21 <juanksanchez21@gmail.com>
2023-10-07 16:42:21 -0600

    Create Proyecto_DesarrolloAplicacionesWebPatrones.java

---
+++ src/test/java/com/Proyecto_DesarrolloAplicacionesWebPatrones/Proyecto_DesarrolloAplicacionesWebPatrones.java
@@ -0,0 +1,13 @@
+package com.Proyecto_DesarrolloAplicacionesWebPatrones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto_DesarrolloAplicacionesWebPatrones {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
