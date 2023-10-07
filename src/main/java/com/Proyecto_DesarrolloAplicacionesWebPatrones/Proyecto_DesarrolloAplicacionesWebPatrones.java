f773d6e03a554f7eb9be7a1384af1295e6696fbc
jcsg21 <juanksanchez21@gmail.com>
2023-10-07 16:41:19 -0600

    Create Proyecto_DesarrolloAplicacionesWebPatrones.java

---
+++ src/main/java/com/Proyecto_DesarrolloAplicacionesWebPatrones/Proyecto_DesarrolloAplicacionesWebPatrones.java
@@ -0,0 +1,13 @@
+package com.Proyecto_DesarrolloAplicacionesWebPatrones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto_DesarrolloAplicacionesWebPatrones {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyecto_DesarrolloAplicacionesWebPatrones.class, args);
+	}
+
+}
